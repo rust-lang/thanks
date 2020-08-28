f5972eaac61d7d85839dea63e34b18aed2d02439
Pietro Albini <pietro@pietroalbini.org>
2020-08-28 17:53:46 +0200

    update reviewers list

--- src/reviewers.rs
+++ src/reviewers.rs
@@ -29,6 +29,7 @@ impl Reviewers {
         let frewsxcv = a("Corey Farwell", "coreyf@rwell.org");
         let gankro = a("Alexis Beingessner", "a.beingessner@gmail.com");
         let guillaumegomez = a("Guillaume Gomez", "guillaume1.gomez@gmail.com");
+        let hanna_kruppe = a("Hanna Kruppe", "hanna.kruppe@gmail.com");
         let huonw = a("Huon Wilson", "wilson.huon@gmail.com");
         let jakub = a("Jakub Kądziołka", "kuba@kadziolka.net");
         let joshtriplett = a("Josh Triplett", "josh@joshtriplett.org");
@@ -36,12 +37,15 @@ impl Reviewers {
         let michaelwoerister = a("Michael Woerister", "michaelwoerister@posteo.net");
         let nikomatsakis = a("Niko Matsakis", "niko@alum.mit.edu");
         let nrc = a("Nick Cameron", "ncameron@mozilla.com");
+        let oli_obk = a("Oliver Scherer", "github35764891676564198441@oli-obk.de");
         let pcwalton = a("Patrick Walton", "pcwalton@mimiga.net");
         let quietmisdreavus = a("QuietMisdreavus", "grey@quietmisdreavus.net");
         let simulacrum = a("Mark Rousskov", "mark.simulacrum@gmail.com");
         let steveklabnik = a("Steve Klabnik", "steve@steveklabnik.com");
         let withoutboats = a("Without Boats", "boats@mozilla.com");
+        let yaahc = a("Jane Lusby", "jlusby42@gmail.com");
         let zoxc = a("John Kåre Alsaker", "john.kare.alsaker@gmail.com");
+        let kodraus = a("Ashley Mannix", "ashleymannix@live.com.au");
 
         map.insert("Aaron1011", a("Aaron Hill", "aa1ronham@gmail.com"));
         map.insert("Aaronepower", a("Erin Power", "xampprocky@gmail.com"));
@@ -57,7 +61,8 @@ impl Reviewers {
         map.insert("JohnTitor", a("Yuki Okushi", "huyuumi.dev@gmail.com"));
         map.insert("JoshTriplett", joshtriplett.clone());
         map.insert("Kimundi", a("Marvin Löbel", "loebel.marvin@gmail.com"));
-        map.insert("KodrAus", a("Ashley Mannix", "ashleymannix@live.com.au"));
+        map.insert("KodrAus", kodraus.clone());
+        map.insert("LukasKalbertodt", a("Lukas Kalbertodt", "lukas.kalbertodt@gmail.com"));
         map.insert("Manishearth", manishearth.clone());
         map.insert("Mark-Simulacru", simulacrum.clone());
         map.insert("Mark-Simulacrum", simulacrum.clone());
@@ -66,11 +71,14 @@ impl Reviewers {
         map.insert("QuietMisdreqvus", quietmisdreavus.clone());
         map.insert("RalfJung", a("Ralf Jung", "post@ralfj.de"));
         map.insert("SimonSapin", a("Simon Sapin", "simon.sapin@exyr.org"));
+        map.insert("tlively", a("Thomas Lively", "tlively@google.com"));
         map.insert("Susurrus", a("Bryant Mairs", "bryant@mai.rs"));
+        map.insert("richkadel", a("Rich Kadel", "richkadel@google.com"));
         map.insert("Thomasdezeeuw", a("Thomas de Zeeu", "thomasdezeeuw@gmail.com"));
         map.insert("TimNN", a("Tim Neumann", "timnn@google.com"));
         map.insert("XAMPPRocky", a("Erin Power", "xampprocky@gmail.com"));
         map.insert("Xanewok", a("Igor Matuszewski", "Xanewok@gmail.com"));
+        map.insert("Yaahc", yaahc.clone());
         map.insert("Zoxc", zoxc.clone());
         map.insert("aatch", aatch.clone());
         map.insert("abonander", a("Austin Bonander", "austin.bonander@gmail.com"));
@@ -117,6 +125,7 @@ impl Reviewers {
         map.insert("dtolnay", a("David Tolnay", "dtolnay@gmail.com"));
         map.insert("durka", a("Alex Durka", "web@alexburka.com"));
         map.insert("dwijnand", a("Dale Wijnand", "dale.wijnand@gmail.com"));
+        map.insert("ecstatic-morse", a("Dylan MacKenzie", "ecstaticmorse@gmail.com"));
         map.insert("eddyb", a("Eduard-Mihai Burtescu", "edy.burt@gmail.com"));
         map.insert("eerden", a("Ercan Erden", "ercerden@gmail.com"));
         map.insert("ehuss", a("Eric Huss", "eric@huss.org"));
@@ -135,6 +144,7 @@ impl Reviewers {
         map.insert("gnzlbg", a("gnzlbg", "gonzalobg88@gmail.com"));
         map.insert("graydon", a("Graydon Hoare", "graydon@pobox.com"));
         map.insert("guillaumegomez", guillaumegomez.clone());
+        map.insert("hanna-kruppe", hanna_kruppe.clone());
         map.insert("huon", huonw.clone());
         map.insert("huonw", huonw.clone());
         map.insert("imperio", guillaumegomez.clone());
@@ -150,14 +160,19 @@ impl Reviewers {
         map.insert("jroesch", a("Jared Roesch", "roeschinc@gmail.com"));
         map.insert("jseyfried", a("Jeffrey Seyfried", "jeffrey.seyfried@gmail.com"));
         map.insert("jsgf", a("Jeremy Fitzhardinge", "jsgf@fb.com"));
+        map.insert("jyn514", a("Joshua Nelson", "jyn514@gmail.com"));
         map.insert("kballard", a("Lily Ballard", "lily@sb.org"));
         map.insert("keeperofdakeys", a("Josh Driver", "keeperofdakeys@gmail.com"));
         map.insert("kennytm", a("kennytm", "kennytm@gmail.com"));
         map.insert("killercup", a("Pascal Hertleif", "killercup@gmail.com"));
+        map.insert("kinnison", a("Daniel Silverstone", "dsilvers@digital-scurf.org"));
         map.insert("kmcallister", a("Keegan McAllister", "mcallister.keegan@gmail.com"));
+        map.insert("kodraus", kodraus.clone());
         map.insert("kornelski", a("Kornel", "kornel@geekhood.net"));
+        map.insert("lcnr", a("Bastian Kauschke", "bastian_kauschke@hotmail.de"));
         map.insert("ljedrz", a("ljedrz", "ljedrz@gmail.com"));
         map.insert("llogiq", a("Andre Bogus", "bogusandre@gmail.com"));
+        map.insert("lqd", a("lqd", "remy.rakic+github@gmail.com"));
         map.insert("lukaramu", a("lukaramu", "lukaramu@users.noreply.github.com"));
         map.insert("luqmana", a("Luqman Aden", "laden@csclub.uwaterloo.ca"));
         map.insert("malbarbo", a("Marco A L Barbosa", "malbarbo@gmail.com"));
@@ -176,13 +191,15 @@ impl Reviewers {
         map.insert("mw", michaelwoerister.clone());
         map.insert("nagisa", a("Simonas Kazlauskas", "git@kazlauskas.me"));
         map.insert("ncr", nrc.clone());
+        map.insert("nellshamrell", a("Nell Shamrell-Harrington", "nellshamrell@gmail.com"));
         map.insert("nick29581", nrc.clone());
         map.insert("nikic", a("Nikita Popov", "nikita.ppv@gmail.com"));
         map.insert("nikomatsakis", nikomatsakis.clone());
         map.insert("nmatsakis", nikomatsakis.clone());
         map.insert("nnethercote", a("Nicholas Nethercote", "nnethercote@mozilla.com"));
         map.insert("nrc", nrc.clone());
-        map.insert("oli-obk", a("Oliver Scherer", "github35764891676564198441@oli-obk.de"));
+        map.insert("oli", oli_obk.clone());
+        map.insert("oli-obk", oli_obk.clone());
         map.insert("ollie27", a("Oliver Middleton", "olliemail27@gmail.com"));
         map.insert("pcwalton", pcwalton.clone());
         map.insert("petrhosek", a("Petr Hosek", "phosek@gmail.com"));
@@ -192,7 +209,7 @@ impl Reviewers {
         map.insert("pnkfelix", a("Felix S Klock II", "pnkfelix@mozilla.com"));
         map.insert("pwalton", pcwalton.clone());
         map.insert("quietmisdreavus", quietmisdreavus.clone());
-        map.insert("rkruppe", a("Robin Kruppe", "robin.kruppe@gmail.com"));
+        map.insert("rkruppe", hanna_kruppe.clone());
         map.insert("sanxiyn", a("Seo Sanghyeon", "sanxiyn@gmail.com"));
         map.insert("scalexm", a("scalexm", "alexandre@scalexm.fr"));
         map.insert("scottmcm", a("Scott McMurray", "scottmcm@users.noreply.github.com"));
@@ -206,6 +223,7 @@ impl Reviewers {
         map.insert("sunfishcode", a("Dan Gohman", "sunfish@mozilla.com"));
         map.insert("thestinger", a("Daniel Micay", "danielmicay@gmail.com"));
         map.insert("tmandry", a("Tyler Mandry", "tmandry@gmail.com"));
+        map.insert("tmiasko", a("Tomasz Miąsko", "tomasz.miasko@gmail.com"));
         map.insert("tromey", a("Tom Tromey", "tom@tromey.com"));
         map.insert("vadimcn", a("Vadim Chugunov", "vadimcn@gmail.com"));
         map.insert("varkor", a("varkor", "github@varkor.com"));
@@ -214,7 +232,8 @@ impl Reviewers {
         map.insert("withoutboats", withoutboats.clone());
         map.insert("wycats", a("Yehuda Katz", "wycats@gmail.com"));
         map.insert("xfix", a("Konrad Borowski", "konrad@borowski.pw"));
-        map.insert("yaahallo", a("Jane Lusby", "jlusby42@gmail.com"));
+        map.insert("yaahallo", yaahc.clone());
+        map.insert("yaahc", yaahc.clone());
         map.insert("yichoi", a("Young-il Choi", "duddlf.choi@samsung.com"));
         map.insert("z0w0", a("Zack Corr", "zack@z0w0.me"));
         map.insert("zackmdavis", a("Zack M. Davis", "code@zackmdavis.net"));
