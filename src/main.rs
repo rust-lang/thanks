110c4dd51567674522a3e1d7d968bdc4de030b10
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2026-03-07 08:31:50 -0800

    mailmap_from_repo(): make `.mailmap` file optional
    If not present, treat the same as an empty file

--- src/main.rs
+++ src/main.rs
@@ -438,13 +438,16 @@ fn build_author_map_(
 }
 
 fn mailmap_from_repo(repo: &git2::Repository) -> Result<Mailmap, Box<dyn std::error::Error>> {
+    let tree = repo.revparse_single("HEAD")?
+        .peel_to_commit()?
+        .tree()?;
+    let file = tree.get_name(".mailmap");
+    let file = match file {
+        None => return Mailmap::from_string("".to_string()),
+        Some(f) => f
+    };
     let file = String::from_utf8(
-        repo.revparse_single("HEAD")?
-            .peel_to_commit()?
-            .tree()?
-            .get_name(".mailmap")
-            .unwrap()
-            .to_object(&repo)?
+        file.to_object(&repo)?
             .peel_to_blob()?
             .content()
             .into(),
