df293e68dc8538d05c35474403bc3a12d6c83a5a
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2026-09-01 16:46:53 -0700

    Rust: always use the "main" branch for nightly
    Now that version logic is part of the `Project` trait and thus specific to a
    project, no need to use fancy detection logic.

--- src/projects.rs
+++ src/projects.rs
@@ -69,16 +69,6 @@ impl Project for Rust {
             .version
             .clone();
 
-        // The nightly branch is the default one, fall back to "main" if it cannot
-        // be read
-        let nightly_branch = match repo.head() {
-            Ok(reference) => match reference.shorthand() {
-                Some(name) => name.to_string(),
-                None => "main".to_string(),
-            },
-            Err(_) => "main".to_string(),
-        };
-
         versions.push(VersionTag {
             name: String::from("Beta"),
             version: {
@@ -103,7 +93,7 @@ impl Project for Rust {
                 last.minor += 2;
                 last
             },
-            raw_tag: nightly_branch,
+            raw_tag: String::from("main"),
             commit: repo
                 .revparse_single("HEAD")
                 .unwrap()
